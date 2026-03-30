e0a154bb7f1b006fc3d73da694f9b7aa3003938a
betopia_ltd <betopia_ltd@DESKTOP-TPA425U>
2026-03-30 16:31:35 +0600

    Module commit

--- TestAutomation/src/automationTeasting/test.java
+++ TestAutomation/src/automationTeasting/test.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -21,32 +22,49 @@ public class test {
 		driver.manage().window().maximize();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // Input field
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.cssSelector("input[type='text']")
-        )).sendKeys("31026");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.cssSelector("#root > div > div:nth-child(1) > div > form > div.auth-wrapper-page > div > div > div.auth-login-body > div > div:nth-child(2) > div > div > div > input")
-        )).sendKeys("betopia2202");
-        driver.findElement(By.cssSelector(".btn")).submit();
+        logIn(driver,wait);
 //        wait.until(ExpectedConditions.elementToBeClickable(
 //                By.xpath("//button")
 //        )).click();
-        wait.until(ExpectedConditions.elementToBeClickable(
-                By.className("item-name")
-        )).click();
-        By searchBox = By.cssSelector("input.ant-select-selection-search-input");
-       wait.until(ExpectedConditions.elementToBeClickable(searchBox)).click();
-       driver.findElement(searchBox).sendKeys("Sup");
 
+        supervisor(driver,wait);
 
-   wait.until(ExpectedConditions.elementToBeClickable(
-       By.xpath("//*[@id=\"30568\"]")
-   )).click();
         String title = driver.getTitle();
         System.out.println("The tiltle of the test website is: "+title);
     	System.out.println("Test Completed Successfully");
         
 	}
+	
+	public static void logIn(WebDriver driver, WebDriverWait wait)
+	{
+        // Input field
+		
+	        wait.until(ExpectedConditions.visibilityOfElementLocated(
+	                By.cssSelector("input[type='text']")
+	        )).sendKeys("31026");
+/**	        WebElement errorMsg = wait.until(
+	        	    ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(),'User id is required')]"))
+	        	);
+	        	System.out.println(errorMsg.getText());**/
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("#root > div > div:nth-child(1) > div > form > div.auth-wrapper-page > div > div > div.auth-login-body > div > div:nth-child(2) > div > div > div > input")
+        )).sendKeys("betopia2202");
+        driver.findElement(By.cssSelector(".btn")).submit();
+	}
+	
+	public static void supervisor(WebDriver driver, WebDriverWait wait) {
+	    wait.until(ExpectedConditions.elementToBeClickable(
+	            By.className("item-name")
+	    )).click();
+	    By searchBox = By.cssSelector("input.ant-select-selection-search-input");
+	   wait.until(ExpectedConditions.elementToBeClickable(searchBox)).click();
+	   driver.findElement(searchBox).sendKeys("Sup");
+	   wait.until(ExpectedConditions.elementToBeClickable(
+		       By.xpath("//*[@id=\"30568\"]")
+		   )).click();
+	}
+
+
 
 }
